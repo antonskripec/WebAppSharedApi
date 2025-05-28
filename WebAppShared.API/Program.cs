18d608abd8f0349746e6bd63792bd430e3b42a2f
antonskripec <anton.skripec@betssongroup.com>
2025-05-28 09:32:08 +0200

    Refactor

--- WebAppShared.API/Program.cs
+++ WebAppShared.API/Program.cs
@@ -1,4 +1,6 @@
 
+using WebAppSharedClassLib;
+
 namespace WebAppShared.API
 {
     public class Program
@@ -8,6 +10,8 @@ namespace WebAppShared.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddSingleton<Calculator>();
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
